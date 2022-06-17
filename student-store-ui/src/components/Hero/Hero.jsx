c0b66fc4e480b4048035777537b18f49801d1327
AndrewValerio <andrewvalerio1396@gmail.com>
2022-06-17 12:04:23 -0700

    added hero banner

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -4,8 +4,17 @@ import "./Hero.css"
 export default function Hero() {
   return (
     <div className="hero">
-        <p className="intro">Welcome!</p>
-        <img className="hero-img"></img>
+    <div className="content">
+    <div className="intro"> 
+    <h7>Welcome!</h7>
+        <h7>Find Your Merch!</h7>
+        <p>We have all kinds of goodies. Click on any of the items to start filling 
+        up your shopping cart. Checkout whenever you're ready.</p>
+        </div>
+        <div className="media">
+        <img className="hero-img" src="https://cdn-icons-png.flaticon.com/512/195/195492.png"/>
+        </div>
+        </div>
     </div>
   )
 }
--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,27 @@
 import * as React from "react"
 import "./Sidebar.css"
+import { BsFillArrowRightCircleFill, BsFillCartPlusFill, BsFillInfoCircleFill } from "react-icons/bs"
+import { MdPayment } from "react-icons/md"
 
 export default function Sidebar() {
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+    <section className="sidebar closed">
+      <div className="wrapper">
+        <button className="toggle-button button closed"><p className="material-icons"><BsFillArrowRightCircleFill /></p></button>
+        <div className="shopping-cart">
+          <div className="cart-icons">
+            <span className="cart-icon icon button">
+              <i className="material-icons"><BsFillCartPlusFill /></i>
+            </span>
+            <span className="cart-icon icon button">
+              <i className="material-icons"><MdPayment /></i>
+            </span>
+            <span className="cart-icon icon button">
+              <i className="material-icons"><BsFillInfoCircleFill /></i>
+            </span>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
