b59f9e21c9dba0d759010c1ea2ff7278106348f2
AndrewValerio <andrewvalerio1396@gmail.com>
2022-06-17 12:30:00 -0700

    added show Description setState

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -1,13 +1,14 @@
 import * as React from "react"
 import "./Hero.css"
+import { BrowserRouter, Link } from "react-router-dom"
 
 export default function Hero() {
   return (
     <div className="hero">
     <div className="content">
     <div className="intro"> 
-    <h7>Welcome!</h7>
-        <h7>Find Your Merch!</h7>
+    <h2>Welcome!</h2>
+        <h2>Find Your Merch!</h2>
         <p>We have all kinds of goodies. Click on any of the items to start filling 
         up your shopping cart. Checkout whenever you're ready.</p>
         </div>
--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -1,9 +1,10 @@
 import * as React from "react"
 import ProductCard from "../ProductCard/ProductCard"
 import "./ProductGrid.css"
+import { useState } from "react"
 
 export default function productGrid(props) {
-  console.log(props)
+  const [showDescription, setShowDescription] = React.useState(false);
     return (
       <div className="productGrid" id="buy">
       <div className="content"><h1>Best Selling Products</h1>
@@ -11,7 +12,7 @@ export default function productGrid(props) {
         <div className="grid">
         {props.products.map((product, i) => {
           return (<ProductCard className="productCard" key={i} name={product.name}
-          price={product.price} pic={product.image}/> )
+          price={product.price} pic={product.image} showDescription={showDescription}/> )
         })}
         </div>
         </div>
