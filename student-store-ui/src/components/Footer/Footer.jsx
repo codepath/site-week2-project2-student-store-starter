ca75736ab54eed11290ad0ca9fb974942641e549
EvansAsuboahStetson <EvansAsuboahStetson@users.noreply.github.com>
2022-06-19 19:37:05 -0400

    message

--- student-store-ui/src/components/Footer/Footer.jsx
+++ student-store-ui/src/components/Footer/Footer.jsx
@@ -11,56 +11,56 @@ export default function Footer() {
   return (
     <div>
       <footer className="footer">
-        <div className="content">
+        <div className="footer-content">
           <div className="top">
             <div className="links">
               <div className="link-column">
                 <h4 className='h4'>Categories</h4>
                 <ul className="ul">
-                  <li>All Categories</li>
-                  <li>Clothing</li>
-                  <li>Food</li>
-                  <li>Accessories</li>
-                  <li>Tech</li>
+                  <li className="lists">All Categories</li>
+                  <li className="lists">Clothing</li>
+                  <li className="lists">Food</li>
+                  <li className="lists">Accessories</li>
+                  <li className="lists">Tech</li>
                 </ul>
               </div>
               <div className="link-column">
                 <h4 className='h4'>Company</h4>
                 <ul className="ul">
-                  <li>About Us</li>
-                  <li>Find a Store</li>
-                  <li>Terms</li>
-                  <li>Sitemap</li>
-                  <li>Careers</li>
+                  <li className="lists">About Us</li>
+                  <li className="lists">Find a Store</li>
+                  <li className="lists">Terms</li>
+                  <li className="lists">Sitemap</li>
+                  <li className="lists">Careers</li>
                 </ul>
               </div>
               <div className="link-column">
                 <h4 className='h4'>Support</h4>
                 <ul className="ul">
-                  <li>Contact Us</li>
-                  <li>Money Refund</li>
-                  <li>Order Status</li>
-                  <li>Shipping Info</li>
-                  <li>Open Dispute</li>
+                  <li className="lists">Contact Us</li>
+                  <li className="lists">Money Refund</li>
+                  <li className="lists">Order Status</li>
+                  <li className="lists">Shipping Info</li>
+                  <li className="lists">Open Dispute</li>
                 </ul>
               </div>
               <div className="link-column">
                 <h4 className='h4'>Account</h4>
                 <ul className="ul">
-                  <li>Login</li>
-                  <li>Register</li>
-                  <li>Account Setting</li>
-                  <li>My Orders</li>
+                  <li className="lists">Login</li>
+                  <li className="lists">Register</li>
+                  <li className="lists">Account Setting</li>
+                  <li className="lists">My Orders</li>
                 </ul>
               </div>
               <div className="link-column">
                 <h4 className='h4'>Socials</h4>
                 <ul className="ul">
-                  <li>Facebook</li>
-                  <li>Twitter</li>
-                  <li>LinkedIn</li>
-                  <li>Instagram</li>
-                  <li>YouTube</li>
+                  <li className="lists">Facebook</li>
+                  <li className="lists">Twitter</li>
+                  <li className="lists">LinkedIn</li>
+                  <li className="lists">Instagram</li>
+                  <li className="lists">YouTube</li>
                 </ul>
               </div>
               <div className="link-column">
@@ -79,6 +79,7 @@ export default function Footer() {
               </div>
             </div>
           </div>
+          
           <div className="bottom">
             <span className="payment-options">
               <img
