77ffc33ded0e9e84fe54b2135d2abbe94ea2af80
Rafael Collado <rco@fb.com>
2022-06-21 13:56:25 -0700

    component organization, eslint

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -1,10 +1,10 @@
-import * as React from "react"
-import "./ShoppingCart.css"
+import * as React from 'react';
+import './ShoppingCart.css';
 
 export default function ShoppingCart() {
   return (
     <div className="shoppingCart">
       <p>ShoppingCart</p>
     </div>
-  )
+  );
 }
