8107713b30f1aff1ded9a3c0b8ee336d02445c3b
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-22 14:24:36 -0700

    Fetch data using axios

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,8 +1,16 @@
 import * as React from "react"
+import axios from "axios"
+import { BrowserRouter } from "react-router-dom"
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import "./App.css"
+
+const API_URL = "https://codepath-store-api.herokuapp.com/store"
+const response = await axios.get(API_URL)
+const products = response.data.products
+
+console.log(products)
 
 export default function App() {
   return (
