f177f81f4239786174f8a771c501663aced69646
AndrewValerio <andrewvalerio1396@gmail.com>
2022-06-22 10:03:27 -0700

    fixed searchbar

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -1,19 +1,92 @@
+// import * as React from "react"
+// import ProductCard from "../ProductCard/ProductCard"
+// import "./ProductGrid.css"
+// import { useState } from "react"
+
+// export default function productGrid(props) {
+//   const [showDescription, setShowDescription] = React.useState(false);
+//     return (
+//       <div className="productGrid" id="buy">
+//       <div className="content"><h1>Best Selling Products</h1>
+//         {console.log(props.products)}
+//         <div className="grid">
+//         {props.products.map((product, i) => {
+//           return (<ProductCard className="productCard" key={i} name={product.name}
+//           price={product.price} pic={product.image} showDescription={showDescription} id={product.id}/> )
+//         })}
+//         </div>
+//         </div>
+//       </div>
+//     )
+// }\
+
 import * as React from "react"
 import ProductCard from "../ProductCard/ProductCard"
 import "./ProductGrid.css"
-import { useState } from "react"
+import { useState } from "react";
 
-export default function productGrid(props) {
-  const [showDescription, setShowDescription] = React.useState(false);
+export default function ProductGrid(props) {
+
+//state variables
+const [searchedActive, setSearchedActive] = useState(false);
+const [searchInput, setSearchInput] = useState("");
+const [filteredData, setFilteredData] = useState([]);
+//prop variables
+const {products} = props;
+const {searched} = props;    
+console.log(props)
+//const {searched} = props;
+const handleOnChange = (e) => {
+    console.log(12,e.target.value);
+    setSearchInput(e.target.value);
+    getFilteredItems(searchInput, products);
+    console.log(35, filteredData);
+    if (e.target.value==''){
+        setSearchedActive(false);
+    }else{
+        setSearchedActive(true);
+    }
+    
+}
+const getFilteredItems = (searchInput, products) => {
+    if (!searchInput){
+        return products;
+    }
+    const data = products.filter((product) => product.name.toLowerCase().includes(searchInput.toLowerCase()));
+    console.log(15, data);
+    setFilteredData(data);
+    // setSearched(true);
+    console.log(16,filteredData);
+    console.log(30,products.filter((product) => product.name.toLowerCase().includes(searchInput.toLowerCase())));
+    return filteredData;
+}
+
+const returnProducts = () => {
+    console.log(111,searchedActive);
+    if (searchedActive === false) {
+        console.log("does this work?");
+        return(props.products.map((product, i) => { 
+            return (<ProductCard className="productCard" key={i} name={product.name}
+            price={product.price} pic={product.image}/> )
+        }))
+    }else if (searchedActive === true){
+        console.log(1113,"does this work?");
+        return(filteredData.map((product, i) => { 
+            return (<ProductCard className="productCard" key={i} name={product.name}
+            price={product.price} pic={product.image}/> )
+        }))
+    }
+}
+  
     return (
       <div className="productGrid" id="buy">
+      <div className="search-input">
+      <input type="text" placeholder="Search products" onChange={handleOnChange} value={searchInput} className="textbox" />
+      </div>
       <div className="content"><h1>Best Selling Products</h1>
         {console.log(props.products)}
-        <div className="grid">
-        {props.products.map((product, i) => {
-          return (<ProductCard className="productCard" key={i} name={product.name}
-          price={product.price} pic={product.image} showDescription={showDescription} id={product.id}/> )
-        })}
+        <div className="grid" >
+        {returnProducts()}
         </div>
         </div>
       </div>
--- student-store-ui/src/components/Searchbar/Searchbar.jsx
+++ student-store-ui/src/components/Searchbar/Searchbar.jsx
@@ -1,39 +1,49 @@
-import "./Searchbar.css"
-import { useState } from "react"
+// import "./Searchbar.css"
+// import { useState } from "react"
+// import { AiOutlineShoppingCart } from "react-icons/ai";
+// import { GiMagnifyingGlass } from "react-icons/gi";
 
-export default function Searchbar(props){
-    //prop variables
-    const {products} = props;
-    //state variables
-    const [searchInput, setSearchInput] = useState("");
-    //searchBar function
-    //<form action="/" method="get">
-       //<label htmlFor="header-search">
-          //<span className="visually-hidden">Search blog posts</span>
-        //</label>
-        //</form><input
-           // type="text"
-           // id="header-search"
-           // placeholder="Search blog posts"
-           // name="s" 
-        ///>
-        //<button type="submit">Search</button>
-    //</form>
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-      };
 
-    if (searchInput.length > 0) {
-          products.filter((product) => {
-          return product.name.match(searchInput);
-      });
-    }
 
-    return(
-        <div className="search-input">
-        <input type="text" placeholder="Search products" onChange={handleChange} value={searchInput} className="textbox" />
-        </div>
-    )
-}
+ export default function SearchBar(){
+//     //prop variables
+//     const {products} = props;
+//     console.log(24,products);
+//     //state variables
+//     const [searchInput, setSearchInput] = useState("");
+//     //searchBar function
+//     // const searchBar = searchInput != "" ? props.product.filter(product) = >
+
+//     const handleChange = (e) => {
+//         e.preventDefault();
+//         setSearchInput(e.target.value);
+//       };
+
+//     if (searchInput.length > 0) {
+//           products.filter((product) => {
+//           return product.name.match(searchInput);
+//       });
+//     }
+
+
+    
+
+//     return(
+
+//         <div className="search-input">
+//             <input id="search" type="text" placeholder="Search products" onChange={(e) => props.setSearchInput(e.target.value)} value={searchInput} className="textbox" />
+
+
+
+//             <div className="catergories">
+//               <button className="all" onClick={(e) => {<Home />}}>All Catergories</button>
+//               <button className="clothing" onClick={(e) => {setSearchInput(e.target.id)}}>Clothing</button>
+//               <button className="food" onClick={(e) => {setSearchInput(e.target.id)}}>Food</button>
+//               <button className="accessoriesll" onClick={(e) => {setSearchInput(e.target.id)}}>Accessories</button>
+//               <button className="tech" onClick={(e) => {setSearchInput(e.target.id)}}>Tech</button>
+//             </div>
+            
+//         </div>
+//     )
+ }
