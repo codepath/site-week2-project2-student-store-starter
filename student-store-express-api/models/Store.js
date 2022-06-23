806004f77cb4c8864796927586b4858cd8b9318e
Annesa Tran <annesat2020@gmail.com>
2022-06-23 15:15:17 -0700

    Added receipts

--- student-store-express-api/models/Store.js
+++ student-store-express-api/models/Store.js
@@ -29,25 +29,33 @@ class Store {
       throw new Error("Request requires both user fields and a non-empty shopping cart")
     }
 
+    // initialize a receipt array
+    let receiptList = []
+
     // get subtotal for the order
     let subtotal = 0
-    for (let i = 0; i < order.length; i++) {
+    for (let i = 0; i < shoppingCart.length; i++) {
       // check if shopping cart array has valid items
-      let item = order[i]
+      let item = shoppingCart[i]
       if ("quantity" in item && "itemId" in item) {
         // get corresponding product item to get price
         const targetItem = await this.fetchProductById(item.itemId)
-        subtotal += (targetItem.price * item.quantity).toFixed(2)
+        let itemCost = parseFloat((targetItem.price * item.quantity).toFixed(2))
+        subtotal += itemCost
+        receiptList = [...receiptList, `${item.quantity} ${targetItem.name} purchased at a cost of $${targetItem.price.toFixed(2)} for a total cost of $${itemCost.toFixed(2)}`]
       } else {
         throw new Error("Request requires all shopping cart items to specify both item ID and quantity")
       }
     }
     
-    const tax = subtotal * 0.0875
+    const tax = parseFloat(subtotal * 0.0875.toFixed(2))
     const total = subtotal + tax
 
+    // update receiptList
+    receiptList = [...receiptList, `Before taxes, the subtotal was $${subtotal.toFixed(2)}`, 
+                                   `After taxes and fees were applied, the total comes out to $${total.toFixed(2)}`]
+
     const purchases = await this.getPurchases()
-    
     // create new purchase object to push to storage and to return as a reponse
     const newPurchase = {
       id: purchases.length + 1,
@@ -55,7 +63,8 @@ class Store {
       email: user.email,
       order: shoppingCart,
       total: total,
-      createdAt: new Date().toISOString()
+      createdAt: new Date().toISOString(),
+      receipt: receiptList
     }
     
     // add the new purchase to storage
