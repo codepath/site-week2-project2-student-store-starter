2bcea32779dda4a37041e96e81cb3bf41eefbbd8
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-23 10:23:02 -0700

    Search Bar styled

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,11 +1,15 @@
 import * as React from "react"
 import "./Home.css"
 import Hero from "../Hero/Hero"
+import ProductGrid from "../ProductGrid/ProductGrid"
+import SearchBar from "../SearchBar/SearchBar"
 
 export default function Home({products, shoppingCart, handleAddItemToCart, handleRemoveItemFromCart}) {
   return (
     <div className="home">
       <Hero />
+      <SearchBar />
+      <ProductGrid />
     </div>
   )
 }
---
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductGrid = () => {
+  return (
+    <div>ProductGrid</div>
+  )
+}
+
+export default ProductGrid
