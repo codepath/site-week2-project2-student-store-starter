acfbfc560301b709294063f01003dfa0cb7a3951
Opebiyi Abolore <sophiaabolore@fb.com>
2022-06-22 21:25:06 -0700

    Fixed the issue with the search bar input not working but changing the placeholder value to a defaultValue instead

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -5,21 +5,23 @@ export default function Hero(products, handleAddItemsToCart, handleRemoveItemToC
   return (
     <div className="hero">
         <section className='banner'>
-          <img src="https://i.pinimg.com/originals/44/3b/44/443b44dfcaf450db93d35c2a5ca5d5c3.gif"></img>
+          <img className="hero-image" src="https://i.pinimg.com/originals/44/3b/44/443b44dfcaf450db93d35c2a5ca5d5c3.gif"></img>
        </section>
     <div id="cover">
       <form method="get" action="">
-    <div className="tb">
-      <div className="td"><input type="text" placeholder="Search" required/></div>
-      <div className="td" id="s-cover">
-        <button type="submit">
-          <div id="s-circle"></div>
-          <span></span>
-        </button>
-      </div>
+        <div className="tb">
+          <div className="td">
+              <input type="text" defaultValue="Search" required/>
+          </div>
+          <div className="td" id="s-cover">
+            <button className="search" type="submit">
+              <div id="s-circle"></div>
+              <span></span>
+            </button>
+          </div>
+        </div>
+      </form>
     </div>
-  </form>
-    </div>
-    </div>
+  </div>
   )
 }
