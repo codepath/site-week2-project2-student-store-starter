4c1052e0a8a3f0ea870ec6ef0a11ac50cf292ca4
Annesa Tran <annesat2020@gmail.com>
2022-06-23 09:38:48 -0700

    Fixed error with shopping cart table setup

--- student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -3,8 +3,6 @@ import "./ShoppingCart.css"
 
 export default function ShoppingCart( {isOpen=false, products=[], shoppingCart=[]} ) {
 
-  console.log(shoppingCart)
-
   // if shoppingCart is empty, render a notification
   if (shoppingCart.length == 0) {
     return (
@@ -28,31 +26,37 @@ export default function ShoppingCart( {isOpen=false, products=[], shoppingCart=[
     <div className="shopping-cart">
       <table className="shopping-cart-table">
         <thead>
-          <th>Name</th>
-          <th>Quantity</th>
-          <th>Cost</th>
-        </thead>
-        {shoppingCart.map(item => 
-          <tr className="cart-row" key={item.itemId}>
-            <td className="cart-product-name">{(products.find(product => product.id === item.itemId)).name}</td>
-            <td className="cart-product-quantity">{item.quantity}</td>
-            <td className="cart-product-cost">{"$"+((products.find(product => product.id === item.itemId)).price * item.quantity).toFixed(2)}</td>
+          <tr>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th>Cost</th>
           </tr>
-        )}
+        </thead>
+        <tbody>
+          {shoppingCart.map(item => 
+            <tr className="cart-row" key={item.itemId}>
+              <td className="cart-product-name">{(products.find(product => product.id === item.itemId)).name}</td>
+              <td className="cart-product-quantity">{item.quantity}</td>
+              <td className="cart-product-cost">{"$"+((products.find(product => product.id === item.itemId)).price * item.quantity).toFixed(2)}</td>
+            </tr>
+          )}
+        </tbody>
       </table>
       <table className="total-table">
-        <tr className="total-table-row">
-          <td className="total-table-label">Subtotal</td>
-          <td className="total-table-value">{"$"+subtotal.toFixed(2)}</td>
-        </tr>
-        <tr className="total-table-row">
-          <td className="total-table-label">Taxes and Fees</td>
-          <td className="total-table-value">{"$"+tax.toFixed(2)}</td>
-        </tr>
-        <tr className="total-table-row">
-          <td className="total-table-label">Total</td>
-          <td className="total-table-value">{"$"+total.toFixed(2)}</td>
-        </tr>
+        <tbody>
+          <tr className="total-table-row">
+            <td className="total-table-label">Subtotal</td>
+            <td className="total-table-value">{"$"+subtotal.toFixed(2)}</td>
+          </tr>
+          <tr className="total-table-row">
+            <td className="total-table-label">Taxes and Fees</td>
+            <td className="total-table-value">{"$"+tax.toFixed(2)}</td>
+          </tr>
+          <tr className="total-table-row">
+            <td className="total-table-label">Total</td>
+            <td className="total-table-value">{"$"+total.toFixed(2)}</td>
+          </tr>
+        </tbody>
       </table>
     </div>
   )
