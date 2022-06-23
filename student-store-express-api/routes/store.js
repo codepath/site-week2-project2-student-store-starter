1d689f1e0b8e02cc62b5c769af04ba26810be9b5
Duy Nguyen <96898896+doowee0509@users.noreply.github.com>
2022-06-23 11:18:27 -0400

    store routing for shopping cart post request

--- student-store-express-api/routes/store.js
+++ student-store-express-api/routes/store.js
@@ -1,70 +1,69 @@
 const express = require("express")
 const Store = require("../models/store")
 const router = express.Router()
-const {NotFoundError, BadRequestError} = require("../utils/errors")
+const { NotFoundError, BadRequestError } = require("../utils/errors")
 
-router.get("/", async (req,res,next) => {
-    try{
+router.get("/", async (req, res, next) => {
+    try {
         const products = await Store.getProducts()
-        return res.status(200).json({products})
+        return res.status(200).json({ products })
     }
-    catch (err){
+    catch (err) {
         next(err)
     }
 })
 
-router.post("/", async (req,res,next) => {
-    try{
+router.post("/", async (req, res, next) => {
+    try {
         const shoppingCart = req.body.shoppingCart
         const user = req.body.user
         const lines = [`Showing receipt for ${user.name} available at ${user.email}:`]
         let total = 0
         if (shoppingCart && user) {
-            shoppingCart.forEach( (item) => {
+            shoppingCart.forEach(async (item) => {
                 if ("quantity" in item && "itemId" in item) {
-                    const product = Store.getProductById(item.itemId)
-                    let itemTotal = item.quantity * product.price
-                    total += itemTotal
-                    lines.push(`${item.quantity} total ${product.name} purchased at a cost of $${item.price.toFixed(2)} for a total cost of $${itemTotal.toFixed(2)}.`)
+                    const product = await Store.getProductById(item.itemId)
+                    let itemTotal = (item.quantity * product.price).toFixed(2)
+                    total += parseFloat(itemTotal)
+                    lines.push(`${item.quantity} total ${product.name} purchased at a cost of $${product.price} for a total cost of $${itemTotal}.`)
                 } else {
                     throw new BadRequestError("each item in the shopping cart should have itemId and quantity field")
                 }
             })
-            lines.push(`Before taxes, the subtotal was $${total}`)
-            total = total * 1.0875
-            lines.push(`After taxes and fees were applied, the total comes out to $${total}`)
 
         } else {
             throw new BadRequestError("shoppingCart or user field not found in request body")
         }
-        const purchases = Store.getPurchases()
-        
+        const purchases = await Store.getPurchases()
+        lines.push(`Before taxes, the subtotal was $${total.toFixed(2)}`)
+        lines.push(`After taxes and fees were applied, the total comes out to $${(total * 1.0875).toFixed(2)}`)
+
         const purchase = {
             id: purchases.length + 1,
             name: user.name,
             email: user.email,
             order: shoppingCart,
-            total: total,
+            total: (total * 1.0875).toFixed(2),
             createdAt: new Date().toISOString(),
-            receipt: {lines: lines}
+            receipt: { lines: lines }
         }
-        return res.status(201).json({"purchase": purchase})
+        return res.status(201).json({ "purchase": purchase })
     }
-    catch (err){
+    catch (err) {
         next(err)
     }
 })
 
 router.get("/:productId", async (req, res, next) => {
     try {
-    const productId = req.params.productId
-    const product = await Store.getProductById(productId)
-    if (!product) {
-        throw new NotFoundError("Transaction not found")
-    }
-    res.status(200).json({ product })
+        const productId = req.params.productId
+        const product = await Store.getProductById(productId)
+        if (!product) {
+            throw new NotFoundError("Transaction not found")
+        }
+        res.status(200).json({ product })
     } catch (err) {
-    next(err)
+        next(err)
     }
 })
 
