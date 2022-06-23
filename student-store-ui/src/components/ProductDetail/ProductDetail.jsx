29d7f3ab3fcdace8533c57577701dd39c20f502e
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-23 10:23:49 -0700

    Access params using useParams

--- student-store-ui/src/components/ProductDetail/ProductDetail.jsx
+++ student-store-ui/src/components/ProductDetail/ProductDetail.jsx
@@ -1,8 +1,11 @@
 import * as React from "react"
+import { useParams } from "react-router-dom"
 import "./ProductDetail.css"
 
 export default function ProductDetail(props) {
+    const { productId } = useParams();
+
     return (
-        <h1>Product detail</h1>
+        <h1>Product detail: { productId }</h1>
     )
 }
