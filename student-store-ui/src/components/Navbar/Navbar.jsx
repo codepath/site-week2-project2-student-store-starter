7b2e58c93b087722b0c00fcfa1ea404fa2cbb4e9
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-23 10:28:49 -0700

    Setup menu skeleton

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import * as React from "react"
+import { Link } from "react-router-dom"
 import "./Navbar.css"
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <ul>
+        <Link to="home">Home</Link>
+        <Link to="about-us">About Us</Link>
+        <Link to="contact-us">Contact Us</Link>
+        <Link to="buy-now">Buy now</Link>
+      </ul>
     </nav>
   )
 }
