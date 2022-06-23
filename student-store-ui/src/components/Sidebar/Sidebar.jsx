bda2392207197ba49983e1d47df66e007f7b6fc7
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-22 19:39:09 -0700

    handleOnToggle props and working

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,11 @@
 import * as React from "react"
 import "./Sidebar.css"
 
-export default function Sidebar({isOpen, shoppingCart, products,checkoutForm}) {
+export default function Sidebar({isOpen, shoppingCart, products,checkoutForm, handleOnToggle, handleOnCheckoutFormChange}) {
   return (
     <section className="sidebar">
       <p>Sidebar</p>
+      <button onClick={handleOnToggle}>HandleOnToggle</button>
     </section>
   )
 }
