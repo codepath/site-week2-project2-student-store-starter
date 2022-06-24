1dfb12b32a194be3f4439726eac322ab6ae01173
Emilio Gonzalez Martinez <emiliogonmar@fb.com>
2022-06-23 22:04:41 -0700

    toggle button

---
+++ student-store-ui/src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./ShoppingCart.css"
+
+const ShoppingCart = () => {
+  return (
+    <div className='shoppingcart'>ShoppingCart</div>
+  )
+}
+
+export default ShoppingCart
--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,12 +1,16 @@
 import * as React from "react"
 import "./Sidebar.css"
 import CheckoutForm from "../CheckoutForm/CheckoutForm"
+import ShoppingCart from "../ShoppingCart/ShoppingCart"
 
 export default function Sidebar({isOpen, shoppingCart, products, checkoutForm, error, successMsg, handleOnToggle, handleCheckoutFormChange, handleOnSubmitCheckoutForm}) {
   return (
-    <section className="sidebar">
+    <section className={`sidebar ${isOpen ? "open":"closed"}`}>
         
         <button onClick={handleOnToggle}>Toggle</button>
+
+        <ShoppingCart 
+          isOpen={isOpen}/>
 
         <CheckoutForm
                 isOpen={isOpen}
