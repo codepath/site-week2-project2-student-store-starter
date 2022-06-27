b89c8d3ba25ac218d1ca5792481ee3ad9a76c03f
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 08:55:06 -0700

    Add hero section

--- student-store-ui/src/components/Hero/Hero.jsx
+++ student-store-ui/src/components/Hero/Hero.jsx
@@ -1,8 +1,22 @@
-import * as React from "react"
-import "./Hero.css"
+import * as React from "react";
+import "./Hero.css";
 
-export default function Hero(props) {
+export default function Hero() {
     return (
-        <h1>Hero</h1>
-    )
+        <div className="hero mb-3 p-5 text-white rounded">
+            <h1 className="display-4">Welcome to the student store!</h1>
+            <p className="lead">
+            We have all kinds of goodies. Click on any of the items to start filling up your shopping cart.
+            </p>
+            <hr className="my-4" />
+            <p>
+            Checkout whenever you're ready.
+            </p>
+            <p className="lead">
+                <a className="btn btn-lg start-buying" href="#buy-now" role="button">
+                    Start buying
+                </a>
+            </p>
+        </div>
+    );
 }
