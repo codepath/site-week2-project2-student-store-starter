66e4c342be4bf14f6da4734536a3dda70cdd4d97
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 14:33:39 -0700

    Make category show active

--- student-store-ui/src/components/Category/Category.jsx
+++ student-store-ui/src/components/Category/Category.jsx
@@ -1,14 +1,16 @@
 import * as React from 'react'
 import './Category.css'
 
-export default function Category({ name, setProducts, allProducts }) {
+export default function Category({ name, setProducts, allProducts, activeCategory, setActiveCategory }) {
     return (
-        <a className='category' onClick={() => {
+        <a className={'category ' + ((activeCategory === name) ? 'active':'') } onClick={() => {
             if (name != 'All categories') {
                 setProducts(allProducts.filter((product) => product.category === name.toLowerCase()))
+                setActiveCategory(name)
             } else {
                 setProducts(allProducts)
+                setActiveCategory('All categories')
             }
-        } }>{ name }</a>
+        } } href="#">{ name }</a>
     )
 }
