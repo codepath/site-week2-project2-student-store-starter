7b77c20b534f45c40e3b2707883f8379d773de0d
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 07:32:49 -0700

    Add bootstrap classes to navbar

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,15 +1,56 @@
-import * as React from "react"
-import "./Navbar.css"
+import * as React from "react";
+import "./Navbar.css";
 
 export default function Navbar() {
-  return (
-    <nav className="navbar">
-      <ul>
-        <a href="#home">Home</a>
-        <a href="#about-us">About Us</a>
-        <a href="#contact-us">Contact Us</a>
-        <a href="#buy-now">Buy now</a>
-      </ul>
-    </nav>
-  )
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <a className="navbar-brand logo" href="#">
+                <i className="bi-cart4"> </i>
+                Student store
+            </a>
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+            >
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <a
+                            className="nav-link active"
+                            aria-current="page"
+                            href="#home"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#about-us">
+                            About us
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#footer">
+                            Contact us
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#buy-now">
+                            Buy now
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
 }
