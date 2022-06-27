328ab8714242df54c57d6049db2cca78584d132b
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 07:59:30 -0700

    Make search bar wide
    add bootstrap classes to do so

--- student-store-ui/src/components/SearchBar/SearchBar.jsx
+++ student-store-ui/src/components/SearchBar/SearchBar.jsx
@@ -1,13 +1,32 @@
-import * as React from "react"
-import "./SearchBar.css"
+import * as React from "react";
+import "./SearchBar.css";
 
 export default function SearchBar({ allProducts, setProducts }) {
     return (
-        <form method="get">
-            <input type="search" name="search-bar" id="searchBar" placeholder="find an item here..." onKeyUp={(e) => {
-                e.preventDefault()
-                setProducts(allProducts.filter((product) => product.name.toLowerCase().includes(e.target.value.toLowerCase())))
-            }} />
+        <form method="get" className="container">
+            <div className="col-auto">
+                <div className="input-group">
+                    <input
+                        type="search"
+                        className="search-bar form-control"
+                        id="searchBar"
+                        placeholder="find an item here..."
+                        onKeyUp={(e) => {
+                            e.preventDefault();
+                            setProducts(
+                                allProducts.filter((product) =>
+                                    product.name
+                                        .toLowerCase()
+                                        .includes(e.target.value.toLowerCase())
+                                )
+                            );
+                        }}
+                    />
+                    <div className="input-group-text">
+                        <i className="bi-search"></i>
+                    </div>
+                </div>
+            </div>
         </form>
-    )
+    );
 }
