c00f927c9373dd8f4c33e043a7401853f3638e6a
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 20:57:43 -0700

    Fix product card alignment

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -8,11 +8,11 @@ export default function ProductCard({ product, onAddClickHandler, onSubtractClic
             <Link to={`products/` + product.id}><img className="card-img-top img-fluid" src={ product.image } alt={ product.name } /></Link>
             <div className="card-body">
             <div className="card-text row mt-3">
-                <div className="col-md-4">
+                <div className="col-md-5">
                     <p>{ product.name }</p>
                     <p>${ product.price }</p>
                 </div>
-                <div className="col-md-3 offset-md-3 p-0">
+                <div className="col-md-4 offset-md-1 p-0">
                     <button onClick={ () => onAddClickHandler(product.id) }>+</button>
                     <button onClick={ () => onSubtractClickHandler(product.id) }>-</button>
                 </div>
