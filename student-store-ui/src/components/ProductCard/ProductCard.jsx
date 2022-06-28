157696d0a792868dff5f2cf369e1ee34268d0a35
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-27 21:38:47 -0700

    Add names as required in readme

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -2,19 +2,21 @@ import * as React from "react"
 import { Link } from "react-router-dom"
 import "./ProductCard.css"
 
-export default function ProductCard({ product, handleAddItemToCart, handleRemoveItemToCart, shoppingCart }) {
+export default function ProductCard({ product, productId, quantity, handleAddItemToCart, handleRemoveItemToCart, showDescription, shoppingCart }) {
     return (
-        <div className="product card col-lg-3 col-md-4 col-sm-6">
-            <Link to={`products/` + product.id}><img className="card-img-top img-fluid" src={ product.image } alt={ product.name } /></Link>
+        <div className="product-card card col-lg-3 col-md-4 col-sm-6">
+            <div className="media">
+                <Link to={`products/` + product.id}><img className="card-img-top img-fluid" src={ product.image } alt={ product.name } /></Link>
+            </div>
             <div className="card-body">
             <div className="card-text row mt-3">
                 <div className="col-md-5">
-                    <p>{ product.name }</p>
-                    <p>${ product.price }</p>
+                    <p className="product-name">{ product.name }</p>
+                    <p className="product-price">${ product.price }</p>
                 </div>
                 <div className="col-md-4 offset-md-1 p-0">
-                    <button onClick={ () => handleAddItemToCart(product.id) }>+</button>
-                    <button onClick={ () => handleRemoveItemToCart(product.id) }>-</button>
+                    <button className="add" onClick={ () => handleAddItemToCart(product.id) }>+</button>
+                    <button className="remove" onClick={ () => handleRemoveItemToCart(product.id) }>-</button>
                 </div>
                 <div className="col-md-2 mt-2 p-0">
                     <ProductCartCount shoppingCart={ shoppingCart } product={ product } />
@@ -29,7 +31,7 @@ export default function ProductCard({ product, handleAddItemToCart, handleRemove
 export function ProductCartCount({ shoppingCart, product }) {
     let prod = shoppingCart.find((prod) => prod['itemId'] === product.id)
     if (prod) {
-        return <span className="cart-count">{ prod['quantity'] }</span>
+        return <span className="cart-count product-quantity">{ prod['quantity'] }</span>
     } else {
         return null
     }
--- student-store-ui/src/components/ProductView/ProductView.jsx
+++ student-store-ui/src/components/ProductView/ProductView.jsx
@@ -6,7 +6,8 @@ export default function ProductView({product, productId, quantity, handleAddItem
     return (
         <div className="product-view">
             <h1 className="product-id">Product #{ productId }</h1>
-            {/* <ProductCard /> */}
+            {/* <ProductCard product={ product } handleAddItemToCart={ handleAddItemToCart }
+                handleRemoveItemToCart={ handleRemoveItemToCart } shoppingCart={ shoppingCart } showDescription={ true } key={`product`+product.id } /> */}
         </div>
     )
 }
