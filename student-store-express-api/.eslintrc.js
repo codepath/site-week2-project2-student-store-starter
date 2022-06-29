b1d8fdcc71b05920cd57d1471ee4428ecad845bc
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-29 10:09:53 -0700

    Add eslint

--- student-store-express-api/.eslintrc.js
+++ student-store-express-api/.eslintrc.js
@@ -1,22 +1,29 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
-  },
-  'extends': [
-    'plugin:react/recommended',
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaFeatures': {
-      'jsx': true,
-    },
-    'ecmaVersion': 'latest',
-  },
-  'plugins': [
-    'react',
-  ],
-  'rules': {
-  },
+	'env': {
+		'browser': true,
+		'commonjs': true,
+		'es2021': true
+	},
+	'extends': 'eslint:recommended',
+	'parserOptions': {
+		'ecmaVersion': 'latest'
+	},
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
 };
