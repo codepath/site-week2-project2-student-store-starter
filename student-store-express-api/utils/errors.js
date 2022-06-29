bd4d54625621ee0d3238640519e5ce736fd2cf26
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-29 10:40:21 -0700

    Add BadRequestError

---
+++ student-store-express-api/utils/errors.js
@@ -0,0 +1,15 @@
+class ExpressError extends Error {
+
+    constructor(message, status) {
+        super();
+        this.message = message;
+        this.status = status;
+    }
+}
+
+class BadRequestError extends ExpressError {
+
+    constructor(message='Bad request error', status=400) {
+        super(message, status);
+    }
+}
