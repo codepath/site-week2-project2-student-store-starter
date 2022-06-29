503db28ebdb547e4233c664ce02ba93dbb9128f2
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-29 10:42:05 -0700

    Export BadRequestError

--- student-store-express-api/utils/errors.js
+++ student-store-express-api/utils/errors.js
@@ -1,15 +1,17 @@
 class ExpressError extends Error {
 
-    constructor(message, status) {
-        super();
-        this.message = message;
-        this.status = status;
-    }
+	constructor(message, status) {
+		super();
+		this.message = message;
+		this.status = status;
+	}
 }
 
 class BadRequestError extends ExpressError {
 
-    constructor(message='Bad request error', status=400) {
-        super(message, status);
-    }
+	constructor(message='Bad request error', status=400) {
+		super(message, status);
+	}
 }
+
+module.exports = BadRequestError;
