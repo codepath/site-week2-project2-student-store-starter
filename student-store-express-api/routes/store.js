22eb3b46d221a2195cb3754f2bbd9598d80774be
Eddy Ogola Onyango <ogolaedd@fb.com>
2022-06-29 11:25:55 -0700

    Handle errors on post to /store

--- student-store-express-api/routes/store.js
+++ student-store-express-api/routes/store.js
@@ -23,12 +23,25 @@ router.get('/:productId', (req, res) => {
 router.post('/', (req, res) => {
 	// receive an object with a `shoppingCart`(each has itemId, quantity fields) array,
 	// and a `user` object(each has name and email fields)
-	// if either is not in the POST body, throw 400 error
-	// if a duplicate of a product in the shoppingCart exists, throw 400 error
-	// if either itemId or quantity is missing for any shoppingCart item, throw 400 error
 	const body = req.body;
+	
+	// if either is not in the POST body, throw 400 error
+	if (!('shoppingCart' in body) || !('user' in body)) {
+		throw new badRequestError('shoppingCart or user not in request body');
+	}
 	const shoppingCart = body.shoppingCart;
 	const user = body.user;
+	// if a duplicate of a product in the shoppingCart exists, throw 400 error
+	// if either itemId or quantity is missing for any shoppingCart item, throw 400 error
+	for (let cartItem of shoppingCart) {
+		if(!('itemId' in cartItem) || !('quantity' in cartItem)) {
+			throw new badRequestError('itemId or quantity missing for some cart item');
+		}
+		const similarIdItems = shoppingCart.filter((item) => item.itemId === cartItem.itemId);
+		if (similarIdItems.length != 1) {
+			throw new badRequestError('multiple similar items in shopping cart');
+		}
+	}
     
 	// calculate total cost for items in shoppingCart
 	let total = 0;
@@ -51,7 +64,7 @@ router.post('/', (req, res) => {
 		createdAt: new Date().toLocaleString('en-US', {'timeZone': 'UTC'})
 	};
 
-	res.send(purchase);
+	res.status(200).send(purchase);
 });
 
 module.exports = router;
