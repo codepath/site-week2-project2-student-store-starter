3d202a6f08e91c20eaae7ffc97779db32bd49926
hayley <71570421+hayley-elise@users.noreply.github.com>
2022-07-15 18:13:53 -0500

    Back-End Finished
    CreatePurchase function does not detect duplicates

--- student-store-express-api/models/store.js
+++ student-store-express-api/models/store.js
@@ -31,56 +31,76 @@ class Store {
     }
 
 
-
     //  creates purchase order & saves it to db
     static async createPurchase(user, shoppingCart) {
-        //  will store & return total before tax
-        let subtotal = 0
         //  will store purchased items
         let purchasedItems = []
-        //  will return user & purchase details
-        let receipt = [`Purchase Details for: ${user.name} - ${user.email}  ~~  `]
         //  creates purchase date
         const createdAt = new Date().toISOString()
         //  creates purchase id
         const purchaseId = storage.get("purchases").value().length + 1
 
+        //  will return receipt containing user & purchase details
+        let receipt = [`~ Purchase Details ~`]
+        receipt.push(`Name: ${user.name} ~ Email: ${user.email}`)
 
-        //  throws 400 error if shopping cart or user fields are missing
-        if (shoppingCart.length == 0 || !user) {
-            throw new BadRequestError("Empty shopping cart/missing fields.")
+
+        //  throws 400 error if shopping cart or user fields are missing/empty
+        if (!user || !user.name || user.name == "" || !user.email || user.email == "") {
+            throw new BadRequestError("User is missing or contains missing/empty fields.")
         }
 
-        //  goes over shopping cart items
-        shoppingCart.forEach((item) => {
-            //  throws 400 error if duplicate items are in the cart
-            if (purchasedItems.includes(item.id)) {
-                throw new BadRequestError("Duplicate cart items found.")
+        if (!shoppingCart || shoppingCart.length == 0) {
+            throw new BadRequestError("Shopping Cart is missing or does not contain any items.")
+        }
+
+
+        //  goes over shopping cart items & update receipt
+        shoppingCart.forEach(async (item) => {
+            //  throws 400 error if item fields are missing/empty
+            if (!item.id || !item.quantity) {
+                throw new BadRequestError("Item id and/or quantity is missing or contains an empty field.")
             }
             
-            //  throws 400 error if shopping cart fields are missing or quantity is negative
-            if (!item.id || item.quantity == 0) {
-                throw new BadRequestError("Missing/empty cart fields.")
-            } else if (item.quantity < 0) {
-                throw new BadRequestError("Invalid quantity.")
+            //  throws 400 error if item id or quantity is 0 or negative
+            if (item.id <= 0 || item.quantity <= 0) {
+                throw new BadRequestError("Item id and/or quantity is invalid.")
             }
 
-
             //  finds item in db products
-            const product = this.fetchProductById(item.id)
+            const product = await this.fetchProductById(item.id)
 
-            //  calculates subtotal of product & its quantity
-            let cost = (product.price * item.quantity)
-            subtotal += cost.toFixed(2)
-
-            //  adds item to array & receipt
+            //  adds cart info to array & receipt
             purchasedItems.push(item)
-            receipt.push(`${product.name} X${item.quantity} ~ Subtotal: $${subtotal} `)
+            receipt.push(`${product.name} ~ X${item.quantity}`)
         })
 
-        //  adds 8.75% tax & pushes it to receipt
-        total = (subtotal * 0.0875).toFixed(2)
-        receipt.push(`~ Total: $${total}`)
+        
+        //  calculates purchase total
+        let total = 0
+
+        for (let i = 0; i < shoppingCart.length; i++) {
+            //  item id field
+            const itemId = shoppingCart[i].id
+            //  quantity field
+            const quantity = shoppingCart[i].quantity
+            //  gets item id in products db
+            const product = await this.fetchProductById(itemId)
+
+            //  total:
+            total += (quantity * product.price)
+        }
+
+        //  adds subtotal (w/o tax) to receipt
+        receipt.push(`Subtotal: $${(total).toFixed(2)}`)
+
+        //  adds tax amount to receipt
+        let tax = (total / 100) * 0.875
+        receipt.push(`Tax amount: $${(tax).toFixed(2)}`)
+
+        //  adds final total (w/ tax) to receipt
+        receipt.push(`~ Total: $${(total + tax).toFixed(2)} ~`)
+
 
         //  creates final purchase object
         const newPurchase = {
@@ -88,13 +108,14 @@ class Store {
             name: user.name,
             email: user.email,
             order: shoppingCart,
-            total: total,
+            total: Number((total + tax).toFixed(2)),
             createdAt: createdAt,
             receipt: receipt
         }
 
         //  pushes object to db
         storage.get("purchases").push(newPurchase).write()
+
 
         return newPurchase
     }
