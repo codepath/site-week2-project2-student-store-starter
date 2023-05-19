e8c92a0984d8d11304aa5b265bb17b0127a75f21
jamjamgobambam <jamila.khawaja85@gmail.com>
2023-05-19 09:38:28 -0400

    fix router error

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import { BrowserRouter } from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
