aef32006105c06c541a4e3a524416b2990049237
Diogo <diogofilipebotelho@gmail.com>
2023-05-26 14:48:51 -0400

    Added NavBar and Logo components.
    Added CheckoutForm file, no code yet.

---
+++ student-store-ui/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Link } from 'react-router-dom'
+
+export default function Logo() {
+  return (
+    <div className="logo">
+      <Link to="/">Logo</Link>
+    </div>
+  )
+}
--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import * as React from "react"
 import "./Navbar.css"
+import { Link } from 'react-router-dom'
+
+import Logo from "../Logo/Logo"
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <Logo/>
+      <Link to="/">Home</Link>
+      <Link to="/About">About Us</Link>
+      <Link to="/Contact">Contact Us</Link>
+      <Link to="/Buy"> Buy Now</Link>
     </nav>
   )
 }
