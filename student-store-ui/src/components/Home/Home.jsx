7b7fd7f95b3a3480753a96f5e3ea8fa626ac762c
Kelechi <kelechi@utexas.edu>
2023-06-12 15:00:31 -0700

    Created home component components

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -1,10 +1,43 @@
 import * as React from "react"
 import "./Home.css"
 
+
+function Hero() {
+  return (
+    <>
+      <h1>Hero Baner</h1>
+    </>
+  )
+}
+function ProductGrid(props) {
+  return (
+    <>
+    <h1>Product Grid Goes Here</h1>
+    </>
+  )
+}
+
+function ProudctCard(props){
+  return (
+    <>
+      <h1>Product card goes here</h1>
+    </>
+  )
+}
+function SearchBar(props){
+  return (
+    <>
+      <h1>Search Bar goes here + filtering</h1>
+    </>
+  )
+  
+}
 export default function Home() {
   return (
     <div className="home">
-      <p>Home</p>
+      <Hero />
+      <SearchBar />
+      <ProductGrid />
     </div>
   )
 }
