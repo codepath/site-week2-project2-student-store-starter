f6421b706fda456c17da46b944f353ebd966a3e0
Nya Haseley-Ayende <nhaseleyayende@salesforce.com>
2023-06-13 10:22:58 -0700

    tuesday morning

--- student-store-ui/src/components/Home/Home.jsx
+++ student-store-ui/src/components/Home/Home.jsx
@@ -8,13 +8,13 @@ export default function Home() {
    
 
     <div className = "hero">
-      <div class="content">
+      <div className="content">
       <div className = "intro">
         <h1> Welcome!!</h1>
         <h1> Find Your Merch!!</h1>
         <p> We have all kinds of goodies. Click on any of the items to start filling up your shopping cart. Checkout whenever you're ready.</p>
       </div>
-      <div class="media">
+      <div className="media">
         <img src="https://codepath-student-store-demo.surge.sh/assets/student_store_icon.18e5d61a.svg" alt="hero" class ="hero-img"></img>
 
       </div>
@@ -23,7 +23,7 @@ export default function Home() {
     </div>
 
 
-    <nav class="sub-navbar">Navigation</nav>
+    <nav className="sub-navbar">Navigation</nav>
 
 
     </div>
