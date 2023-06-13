1850fc4917a9b1ac792ea982d36bb4740d50c6c5
pierrelasaine <92757282+pierrelasaine@users.noreply.github.com>
2023-06-13 11:46:52 -0700

    laying scaffolding

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,19 +1,39 @@
 import * as React from "react"
-import { BrowserRouter } from 'react-router-dom'
-import Navbar from "../Navbar/Navbar"
+import { useEffect, useState } from "react"
+import { BrowserRouter } from "react-router-dom"
+import axios from "axios"
 import Sidebar from "../Sidebar/Sidebar"
+import Navbar from "../Navbar/Navbar"
+import Subnavbar from "../Subnavbar/Subnavbar"
 import Home from "../Home/Home"
 import "./App.css"
 
+const mockBackend = "https://codepath-store-api.herokuapp.com/store"
+
 export default function App() {
+
+  const [products, setProducts] = useState()
+
+  // useEffect(() => {
+  //   axios.get(mockBackend)
+  //     .then(response => {
+  //       setProducts(response.data.products)
+  //     })
+  // }, [products])
+
   return (
     <div className="app">
       <BrowserRouter>
         <main>
-          {/* YOUR CODE HERE! */}
-          <Navbar />
           <Sidebar />
-          <Home />
+          <section className="page">
+            <Navbar />
+            <div className="hero">
+              <h1>Header</h1>
+            </div>
+            <Subnavbar />
+            <Home />
+          </section>
         </main>
       </BrowserRouter>
     </div>
---
+++ student-store-ui/src/components/Subnavbar/Subnavbar.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import "./Subnavbar.css"
+
+export default function Subnavbar() {
+  return (
+    <section className="subnavbar">
+      <p>Subnavbar</p>
+    </section>
+  )
+}
