6179a92bd218002f78821fc4d4a27329ffab69bc
Kelechi <kelechi@utexas.edu>
2023-06-14 09:45:34 -0700

    added sidebar (WIP)

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,10 +1,28 @@
-import * as React from "react"
-import "./Sidebar.css"
+import * as React from "react";
+import { useState } from "react";
+import "./Sidebar.css";
 
 export default function Sidebar() {
+  function SideBarShoppingCart(){
+    return (
+      <>
+      </>
+    )
+  }
+  function SideBarCheckoutForm(){
+    return (
+      <>
+      </>
+    )
+  }
+  
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <section className="sidebar">
-      <p>Sidebar</p>
+    <section className={"sidebar-container " + (isOpen ? "open" : "")}>
+      <button className={"sidebar-toggle-button" + (isOpen ? "open" : "")}>
+        <i className="material icons classnae here"> </i>
+      </button>
+      {}
     </section>
   )
 }
