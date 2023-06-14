076eb4aefc54ecd4aae95c6dc830a4bc965e8af6
Marwan Kabir <mkabir@mkabir-ltmwxcp.internal.salesforce.com>
2023-06-14 14:34:28 -0700

    updated again

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,23 +1,33 @@
 import * as React from "react"
 import axios from 'axios'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route} from 'react-router-dom'
 import Navbar from "../Navbar/Navbar"
 import Sidebar from "../Sidebar/Sidebar"
 import Home from "../Home/Home"
 import ShoppingCart from "../ShoppingCart/ShoppingCart"
 import { useState } from "react"
+import { useEffect } from "react"
 import "./App.css"
 
+
 export default function App() {
+  //Url for API
+  const url = "https:://codepath-store-api.herokuapp.com/store"
+  //useState
+  const[products, setProducts] = useState();
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setProducts(response.data.products)
+    })
+  }, [])
+
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path = "/" element={<Home/>}/>
-          <Route path = "/about" element={<About/>}/>
-        </Routes>
         <main>
+          <Navbar />
           <Sidebar />
+          <Home products = {products}/>
         </main>
       </BrowserRouter>
     </div>
--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,17 @@
 import * as React from "react"
+// import { AiOutlineInstagram, BsTwitter, BsFacebook } from "react-icons/fa";
 import "./Navbar.css"
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <p>Navbar</p>
+      <ul>
+        <li></li>
+        <li><a href = "/" className="site-home">Home</a></li>
+        <li><a href = "/#About">About</a></li>
+        <li><a href = "/#Contact">Contact Us</a></li>
+        <li><a href = "/#Buy">Buy Now</a></li>
+      </ul>
     </nav>
   )
 }
