159c80f66a0d6a7496e15a8b9b33b47659988d2f
Kelechi <kelechi@utexas.edu>
2023-06-14 09:43:07 -0700

    added fetch call to get products + minor css updates

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -1,20 +1,30 @@
 import * as React from "react";
+import {useEffect, useState } from "react";
 import { BrowserRouter } from 'react-router-dom';
 import Navbar from "../Navbar/Navbar";
 import Sidebar from "../Sidebar/Sidebar";
 import Home from "../Home/Home";
 import "./App.css";
 import Footer from "../Footer/Footer";
+import axios from "axios";
 
 export default function App() {
+
+  const URL_DB = "https://codepath-store-api.herokuapp.com/store";
+  const [products, setProduts] = useState([]);
+  useEffect(() => {
+    axios.get(URL_DB).then((response)=>{
+      setProduts(() => response.data.products);
+    })
+
+  }, []);
   return (
-    <div className="app">
+    <div className="app-container">
       <BrowserRouter>
-        <main>
-          {/* YOUR CODE HERE! */}
+        <main className="app-content">
           <Navbar />
           <Sidebar />
-          <Home />
+          <Home products={products}/>
           <Footer />
         </main>
       </BrowserRouter>
