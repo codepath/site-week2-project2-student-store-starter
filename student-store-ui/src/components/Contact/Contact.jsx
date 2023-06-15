2f66058e7a91bbd81e28f8eea27cde60dd9cd479
Kelechi <kelechi@utexas.edu>
2023-06-15 08:17:43 -0700

    created contact componenet

---
+++ student-store-ui/src/components/Contact/Contact.jsx
@@ -0,0 +1,14 @@
+import "./Contact.css";
+import Socials from "../Socials/Socials";
+
+export default function Contact(){
+    let contactObject = {
+        Email : "code@path.org",
+        Phone : "1-800-CODEPATH",
+        Address : "123 Fake Street, San Francisco, CA",
+    }
+
+    return (
+        <p>contact us page</p>
+    )
+}
