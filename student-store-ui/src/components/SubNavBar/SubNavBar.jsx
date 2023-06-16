db60a07ff934539d323f7fb31928dfc36bc12058
Ana Camba Gomes <acamba@acamba-ltmya7c.internal.salesforce.com>
2023-06-16 08:32:18 -0700

    layout almost

--- student-store-ui/src/components/SubNavBar/SubNavBar.jsx
+++ student-store-ui/src/components/SubNavBar/SubNavBar.jsx
@@ -43,64 +43,138 @@ function handleCategoryClick(category) {
 
 
 return (
-    <div className="Subnavbar">
-        <div className="content">
+    // <div className="Subnavbar">
+    //     <div className="content">
 
-            <div className="row">
+    //         <div className="row">
 
-                <div className="search-bar">
+    //             <div className="search-bar">
 
-                    <form id="search-form">
-                        <label htmlFor="search"></label>
-                        <input id="search-input" type="text" name="search" placeholder="Search" value={query} onChange={handleChange} />
-
-
-                    </form>
-                    <i className="material-icons">search</i>
-
-                </div>
+    //                 <form id="search-form">
+    //                     <label htmlFor="search"></label>
+    //                     <input id="search-input" type="text" name="search" placeholder="Search" value={query} onChange={handleChange} />
 
 
-                <div className="icons">
-                    <span className="help"><i className="material-icons">help</i>Help</span>
-                    <span className="cart">
-                        <a href="/">My Cart<i className="material-icons">shopping_cart</i></a>
-                    </span>
-                </div>
+    //                 </form>
+    //                 <i className="material-icons">search</i>
 
-            </div>
-            <div className="row">
-                <div className="menu">
-                    <i className="material-icons">menu</i>
-                </div>
-                <ul className="menu-open">
-                    <li className={activeCategory === "All Categories" ? "isActive" : ""}>
-
-                        <button onClick={() => handleCategoryClick("All Categories")}>All Categories</button>
-                    </li>
-                    <li className={activeCategory === "Clothing" ? "isActive" : ""}>
-                        <button onClick={() => handleCategoryClick("Clothing")}>Clothing</button>
-                    </li>
-                    <li className={activeCategory === "Food" ? "isActive" : ""}>
-                        <button onClick={() => handleCategoryClick("Food")}>Food</button>
-                    </li >
-                    <li className={activeCategory === "Accessories" ? "isActive" : ""}>
-                        <button onClick={() => handleCategoryClick("Accessories")}>Accessories</button>
-                    </li>
-                    <li className={activeCategory === "Tech" ? "isActive" : ""}>
-                        <button onClick={() => handleCategoryClick("Tech")}>Tech</button>
-                    </li>
-                </ul>
+    //             </div>
 
 
+    //             <div className="icons">
+    //                 <span className="help"><i className="material-icons">help</i>Help</span>
+    //                 <span className="cart">
+    //                     <a href="/">My Cart<i className="material-icons">shopping_cart</i></a>
+    //                 </span>
+    //             </div>
+
+    //         </div>
+    //         <div className="row">
+    //             <div className="menu">
+    //                 <i className="material-icons">menu</i>
+    //             </div>
+    //             <ul className="menu-open">
+    //                 <li className={activeCategory === "All Categories" ? "isActive" : ""}>
+
+    //                     <button onClick={() => handleCategoryClick("All Categories")}>All Categories</button>
+    //                 </li>
+    //                 <li className={activeCategory === "Clothing" ? "isActive" : ""}>
+    //                     <button onClick={() => handleCategoryClick("Clothing")}>Clothing</button>
+    //                 </li>
+    //                 <li className={activeCategory === "Food" ? "isActive" : ""}>
+    //                     <button onClick={() => handleCategoryClick("Food")}>Food</button>
+    //                 </li >
+    //                 <li className={activeCategory === "Accessories" ? "isActive" : ""}>
+    //                     <button onClick={() => handleCategoryClick("Accessories")}>Accessories</button>
+    //                 </li>
+    //                 <li className={activeCategory === "Tech" ? "isActive" : ""}>
+    //                     <button onClick={() => handleCategoryClick("Tech")}>Tech</button>
+    //                 </li>
+    //             </ul>
 
 
-            </div>
 
 
-        </div>
+    //         </div>
 
 
-    </div>
+    //     </div>
+
+
+    // </div>
+
+    <nav className="sub-navbar">
+   <div className="content">
+      <div className="row">
+         <div className="search-bar"><input id="search-input" type="text" name="search" placeholder="Search" value={query} onChange={handleChange}/><i className="material-icons">search</i></div>
+         <div className="links">
+            <span className="help"><i className="material-icons">help</i>Help</span>
+            <div className="cart"><a href="/">My Cart<i class="material-icons">shopping_cart</i></a></div>
+         </div>
+      </div>
+      <div className="row">
+         <div className="hamburger-menu"><i className="material-icons">menu</i></div>
+         <ul className="category-menu open">
+                <li className={activeCategory === "All Categories" ? "isActive" : ""}>
+
+                    <button onClick={() => handleCategoryClick("All Categories")}>All Categories</button>
+                </li>
+                <li className={activeCategory === "Clothing" ? "isActive" : ""}>
+                    <button onClick={() => handleCategoryClick("Clothing")}>Clothing</button>
+                </li>
+                <li className={activeCategory === "Food" ? "isActive" : ""}>
+                    <button onClick={() => handleCategoryClick("Food")}>Food</button>
+                </li >
+                <li className={activeCategory === "Accessories" ? "isActive" : ""}>
+                    <button onClick={() => handleCategoryClick("Accessories")}>Accessories</button>
+                </li>
+                <li className={activeCategory === "Tech" ? "isActive" : ""}>
+                    <button onClick={() => handleCategoryClick("Tech")}>Tech</button>
+              </li>
+         </ul>
+      </div>
+   </div>
+</nav> 
+
+
+
+
+
+
 )
 }
+
+// second try:
+
+//  <nav className="sub-navbar">
+//    <div className="content">
+//       <div className="row">
+//          <div className="search-bar"><input id="search-input" type="text" name="search" placeholder="Search" value={query} onChange={handleChange}/><i className="material-icons">search</i></div>
+//          <div className="links">
+//             <span className="help"><i className="material-icons">help</i>Help</span>
+//             <div className="cart"><a href="/">My Cart<i class="material-icons">shopping_cart</i></a></div>
+//          </div>
+//       </div>
+//       <div className="row">
+//          <div className="hamburger-menu"><i className="material-icons">menu</i></div>
+//          <ul className="category-menu open">
+//                 <li className={activeCategory === "All Categories" ? "isActive" : ""}>
+
+//                     <button onClick={() => handleCategoryClick("All Categories")}>All Categories</button>
+//                 </li>
+//                 <li className={activeCategory === "Clothing" ? "isActive" : ""}>
+//                     <button onClick={() => handleCategoryClick("Clothing")}>Clothing</button>
+//                 </li>
+//                 <li className={activeCategory === "Food" ? "isActive" : ""}>
+//                     <button onClick={() => handleCategoryClick("Food")}>Food</button>
+//                 </li >
+//                 <li className={activeCategory === "Accessories" ? "isActive" : ""}>
+//                     <button onClick={() => handleCategoryClick("Accessories")}>Accessories</button>
+//                 </li>
+//                 <li className={activeCategory === "Tech" ? "isActive" : ""}>
+//                     <button onClick={() => handleCategoryClick("Tech")}>Tech</button>
+//               </li>
+//          </ul>
+//       </div>
+//    </div>
+// </nav> 
