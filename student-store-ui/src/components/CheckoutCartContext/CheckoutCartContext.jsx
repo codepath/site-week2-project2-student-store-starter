48feb956250199b3f6d5c7d4ec2823dd609d5113
Kelechi <kelechi@utexas.edu>
2023-06-17 17:32:43 -0700

    added cart context and reducer

---
+++ student-store-ui/src/components/CheckoutCartContext/CheckoutCartContext.jsx
@@ -0,0 +1,4 @@
+import { createContext } from "react";
+// creating context for reducer
+export const CheckoutCartContext = createContext(null);
+export const CheckoutCartDispatchContext = createContext(null);
