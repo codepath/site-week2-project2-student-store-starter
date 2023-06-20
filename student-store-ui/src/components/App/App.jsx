cd28e2d4452196330217be30c615c295b0256492
Marley <marleyburrows1@gmail.com>
2023-06-20 13:29:40 -0700

    updated footer

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -39,10 +39,78 @@ export default function App() {
       </Routes>
       
       <Sidebar isOpen={sidebarOpen} toggleSidebar={handleSidebarToggle} />
-        <footer>
+        {/* <footer>
         <p>Author: Marley Burrows</p>
         <p><a href="mailto:mburrows@salesforce.com">mburrows@salesforce.com</a></p>
+      </footer> */}
+
+      <footer class="footer">
+        <div class="content">
+          <div class="top">
+            <div class="links">
+              <div class="link-column">
+                <h4>Categories</h4>
+                <ul>
+                  <li>All Categories</li>
+                <li>Clothing</li>
+                <li>Food</li>
+                <li>Accessories</li>
+                <li>Tech</li>
+                </ul>
+                </div>
+              <div class="link-column">
+                <h4>Company</h4>
+                <ul>
+                  <li>About Us</li>
+                  <li>Find a Store</li>
+                  <li>Terms</li><li>Sitemap</li>
+                  <li>Careers</li>
+                </ul>
+                </div>
+              <div class="link-column">
+                <h4>Support</h4>
+                <ul>
+                  <li>Contact Us</li>
+                  <li>Money Refund</li>
+                  <li>Order Status</li>
+                  <li>Shipping Info</li>
+                  <li>Open Dispute</li>
+                  </ul>
+                </div>
+              <div class="link-column">
+                <h4>Account</h4>
+                <ul><li>Login</li>
+                <li>Register</li>
+                <li>Account Setting</li>
+                <li>My Orders</li>
+                </ul>
+                </div>
+              <div class="link-column">
+                <h4>Socials</h4>
+                <ul>
+                  <li>Facebook</li>
+                  <li>Twitter</li>
+                  <li>LinkedIn</li>
+                  <li>Instagram</li>
+                  <li>YouTube</li>
+                  </ul>
+                </div>
+              <div class="link-column">
+                <h4>Our App</h4>
+                <ul>
+                  <li><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Download_on_the_App_Store_Badge.svg/1200px-Download_on_the_App_Store_Badge.svg.png" alt="app store"></img></li>
+                  <li><img src="https://swanlibraries.net/wp-content/uploads/2019/03/google-play_modified-1024x301.png" alt="google play store"></img></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+            <p>Author: Marley Burrows <a href="mailto:mburrows@salesforce.com">mburrows@salesforce.com</a></p>
+                </div>
       </footer>
+
+
+
+
       </main>
       </BrowserRouter>
     </div>
