6dcddbaabdb5c6f737bd7c6488b093c3f55a2ed4
Habeebah Muse <hmuse@hmuse-ltm6cpu.internal.salesforce.com>
2023-06-21 16:30:01 -0700

    sidebar

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,44 @@
 import * as React from "react";
 import "./Sidebar.css";
 import { Link } from "react-router-dom";
-import { FaBars } from "react-icons/fa";
+import { useState } from "react";
+// import Cancel, { colorPalette } from "material-icons-react";
+// import { BiSolidCartAdd } from "react-icons/fa";
+// // import { AiOutlineShoppingCart } from "react-icons/fa";
+<link
+  rel="stylesheet"
+  href="https://fonts.googleapis.com/icon?family=Material+Icons"
+></link>;
 
-export default function Sidebar({
-  isOpen,
-  shoppingCart,
-  products,
-  checkoutForm,
-  handleOnCheckoutFormChange,
-  handleOnSubmitCheckoutForm,
-  handleOnToggle,
-}) {
+export default function Sidebar({}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const openTable = (
+    <>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Quantity</th>
+          <th>Unit Price</th>
+          <th>Cost</th>
+        </tr>
+      </table>
+      <h3>Payment Info</h3>
+      <form>
+        <label for="name">Name</label>
+        <input type="text" id="name" name="name"></input>
+        <label for="email">Email</label>
+        <input type="text" id="email" name="email"></input>
+        <button className="checkout-btn">Checkout</button>
+      </form>
+    </>
+  );
+
   return (
-    <section className="sidebar">
-      <Link to="#" className="menu-bar">
-        <FaBars />
-      </Link>
+    <section className={isOpen ? "sidebar open" : "sidebar closed"}>
+      <h3>Shopping Cart</h3>
+      <button onClick={() => setIsOpen(!isOpen)}>Toggle</button>
+      {isOpen ? openTable : null}
     </section>
   );
 }
