f63ec063f5eaaad30b45eeb01888e8f1c0e02691
AveryMyart <myart.avery@gmail.com>
2023-06-21 10:33:43 -0700

    update

--- student-store-ui/src/components/App/App.jsx
+++ student-store-ui/src/components/App/App.jsx
@@ -8,33 +8,35 @@ import Hero from "../Hero/Hero"
 import SubNavbar from "../SubNavbar/SubNavbar"
 import { useEffect } from "react"
 import axios from 'axios'
-import {useState} from "react"
+import { useState } from "react";
 
 export default function App() {
-
-const url = "https://codepath-store-api.herokuapp.com/store"
-const [products, setProducts] = useState([])
+  const url = "https://codepath-store-api.herokuapp.com/store";
+  const [products, setProducts] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     axios.get(url).then((response) => {
-      setProducts(response.data.products)
-    })
-  }, [])
-
-  
+      setProducts(response.data.products);
+    });
+  }, []);
 
   return (
     <div className="app">
       <BrowserRouter>
         <main>
-          {/* YOUR CODE HERE! */}
           <Navbar />
           <Hero />
-          <SubNavbar />
+          <SubNavbar setSelectedCategory={setSelectedCategory} />
           <Sidebar />
-          <Home products={products} />
+          <Home 
+            products={products} 
+            selectedCategory={selectedCategory} 
+            searchTerm={searchTerm}
+          />
         </main>
       </BrowserRouter>
     </div>
-  )
+  );
 }
