5836d161c7a3b0c48ff43544ce710aaf0cce2d83
Kelechi <kelechi@utexas.edu>
2023-06-21 13:26:53 -0700

    added cors and express.json() middleware

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,14 +1,19 @@
 const morgan = require("morgan");
 const express = require("express");
+const cors = require("cors");
+const app = express();
 const storeRouter = require("./routes/store")
 const { NotFoundError } = require("./utils/errors")
 
 app.use(morgan("tiny")); // for console logs
+app.use(cors()); // for localhost frontend to make requests to localhost backend
+app.use(express.json()); // converts body to json
 
-app.use(express.json());
 app.use(`/store`, storeRouter);
+
+// DEFAULT ROUTE
 app.get("/", async (request, response) => {
-    response.send({"ping" : "pong"})
+    response.status(200).send({"ping" : "pong"})
 })
 
 /* Handle all 404 errors that weren't matched by a route */
@@ -19,7 +24,7 @@ app.use((req, res, next) => {
   /* Generic error handler - anything that is unhandled will be handled here */
 app.use((error, req, res, next) => {
     const status = error.status || 500
-    const message = error.message
+    const message = error.message || "Something went wrong in the application"
   
     return res.status(status).json({
       error: { message, status },
