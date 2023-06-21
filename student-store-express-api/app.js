2d0115a45c598c9807865fd291072e4123b3a1c4
Iden Amoako <iamoako@iamoako-ltmuqpv.internal.salesforce.com>
2023-06-21 14:01:12 -0700

    updated api

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,18 @@
 // YOUR CODE HERE
+const express = require('express')
+const cors = require('cors')
+const db = require("./data/db.json")
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.status(200)
+    res.send(db)
+})
+
+
+module.exports = app
