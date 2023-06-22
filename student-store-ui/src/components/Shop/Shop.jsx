f6e8428d722afb2da5263cc9a30df3c0a9de44cc
Kelechi <kelechi@utexas.edu>
2023-06-22 14:00:23 -0700

    added better styling to product grid

--- student-store-ui/src/components/Shop/Shop.jsx
+++ student-store-ui/src/components/Shop/Shop.jsx
@@ -36,13 +36,15 @@ function ProductGrid({ products, filter }) {
             <div className="product-grid">
                 {/* // renders each product */}
                 {filteredProducts.map((product) => {
-                    
 
                     return (
                         <div className="product">
                             <Link to={`/products/${product.id}`}>
                                 <img src={product.image} alt={"image of" + product.name} />
-                                <p className="product-title">{product.name}</p>
+                                <div className="product-header">
+                                    <p className="product-title">{product.name}</p>
+                                    <p className="product-price">${product.price.toFixed(2)}</p>
+                                </div>
                             </Link>
                             <div className="product-buttons">
                                 <button type="button" id="increment" onClick={(e) => handleProductButtonOnClick(e, product)} className="add">
@@ -52,8 +54,6 @@ function ProductGrid({ products, filter }) {
                                     <i id="decrement" className="material-icons">remove</i>
                                 </button>
                             </div>
-                            <div className="prices"></div>
-                            <p className="product-price">${product.price.toFixed(2)}</p>
                         </div>
 
 
@@ -74,7 +74,7 @@ function SearchBar({ updateQuery, search }) {
                 value={search} onChange={updateQuery}
                 label="Search" placeholder="Search Products"
                 className="search-input" />
-            <i className="material-icons">search</i>
+            <i className="material-icons">help</i>
         </div>
     )
 }
@@ -101,7 +101,7 @@ function FilterBar({ updateQuery, activeCategory }) {
 export default function Shop() {
     // Component displays product grids
     // with search/filter bars
-    const URL_DB = "https://codepath-store-api.herokuapp.com/store";
+    const URL_DB = "http://localhost:3001/store";
     const [products, setProducts] = useState([]);
     const [query, setQuery] = useState({
         category: "All",
@@ -112,6 +112,8 @@ export default function Shop() {
         axios.get(URL_DB).then((response) => {
             console.log("fetching data...");
             setProducts(() => response.data.products);
+        }).catch((e) => {
+            console.log("Error: ", e)
         })
     }, []);
     function queryUpdateHandler(event) {
