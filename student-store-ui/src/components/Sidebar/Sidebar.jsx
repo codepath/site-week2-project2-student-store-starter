19dfe3d6ed1ba29801e6314a3ac399a889b5acdd
Kelechi <kelechi@utexas.edu>
2023-06-22 14:00:06 -0700

    centered sidebar toggle button

--- student-store-ui/src/components/Sidebar/Sidebar.jsx
+++ student-store-ui/src/components/Sidebar/Sidebar.jsx
@@ -4,12 +4,13 @@ import "./Sidebar.css";
 import CheckoutCartForm from "../CheckoutCartForm/CheckoutCartForm";
 import CartItemsTable from "../CartItemsTable/CartItemsTable.jsx";
 export default function Sidebar() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <section className={"sidebar-container " + (isOpen ? "open" : "")}>
-      <button onClick={() => setIsOpen((open) => !open)} className={"sidebar-toggle-button " + (isOpen ? "open" : "")}>
-        <i className="material-symbols-outlined">{isOpen ? "arrow_back_ios" : "shopping_cart"}</i>
-      </button>
+        <button onClick={() => setIsOpen((open) => !open)} className={"sidebar-toggle-button " + (isOpen ? "open" : "")}>
+          <i className="material-symbols-outlined">{isOpen ? "arrow_back_ios" : "shopping_cart"}</i>
+        </button>
+      
       {isOpen && (
         <>
           <CartItemsTable />
