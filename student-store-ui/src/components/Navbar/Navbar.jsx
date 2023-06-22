e6f3ce39045381d3a4993da9e917b76473ea0deb
Godfred Asamoah <realswat@meta.com>
2023-06-21 22:42:30 -0700

    initial thoughts

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -1,10 +1,22 @@
-import * as React from "react"
-import "./Navbar.css"
+import * as React from 'react';
+import '../../globals.css';
+import CodePathLogo from '../../assets/images/codepathLogo.png';
+import { listItem } from '../../assets/styles';
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      <p>Navbar</p>
-    </nav>
-  )
+    <div className='flex px-2 justify-around items-center w-full h-100px bg-green-700'>
+      <img
+        src={CodePathLogo}
+        alt='CodePath logo'
+        className='h-[85px] cursor-pointer'
+      />
+      <ul className='flex justify-around items-center w-1/3'>
+        <li className={listItem}>Home</li>
+        <li className={listItem}>About Us</li>
+        <li className={listItem}>Contact Us</li>
+        <li className={listItem}>Buy Now</li>
+      </ul>
+    </div>
+  );
 }
