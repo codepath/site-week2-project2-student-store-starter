e3f2fe5cc23899f9c9afd65034a697a54a904225
Godfred Asamoah <realswat@meta.com>
2023-06-22 09:19:15 -0700

    initial thought

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,22 @@
 // YOUR CODE HERE
+import * as React from "react"
+import { BrowserRouter } from 'react-router-dom'
+import Navbar from "../Navbar/Navbar"
+import Sidebar from "../Sidebar/Sidebar"
+import Home from "../Home/Home"
+import "./App.css"
+
+export default function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <main>
+          {/* YOUR CODE HERE! */}
+          <Navbar />
+          <Sidebar />
+          <Home />
+        </main>
+      </BrowserRouter>
+    </div>
+  )
+}
