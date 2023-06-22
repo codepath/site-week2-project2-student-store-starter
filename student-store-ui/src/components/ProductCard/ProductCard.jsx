4b753769d71f9c2fdaab4a132aff78d9a38acc4e
AveryMyart <myart.avery@gmail.com>
2023-06-21 18:42:36 -0700

    update

--- student-store-ui/src/components/ProductCard/ProductCard.jsx
+++ student-store-ui/src/components/ProductCard/ProductCard.jsx
@@ -1,6 +1,19 @@
 import * as React from "react"
 import "./ProductCard.css"
 
-export default function ProductCard() {
-
-}
+export default function ProductCard({ name, id, image, price }) {
+    console.log(id);
+    return (
+      <section className="product">
+        <img className="product-image" src={image} />
+        <div className="main-info">
+          <p>
+            <b>{name}</b>
+          </p>
+          <p>
+            <b>{price}</b>
+          </p>
+        </div>
+      </section>
+    );
+  }
