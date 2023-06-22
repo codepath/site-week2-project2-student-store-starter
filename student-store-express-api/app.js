df493db6d9e9a67e89faa15cc31c8af83843a164
Maria Eduarda Tavora Carneiro <mtavoracarneiro@mtavora-ltm8c6x.internal.salesforce.com>
2023-06-22 10:50:40 -0700

    Added basic routing to API

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,17 @@
-// YOUR CODE HERE
+const express = require('express')
+const cors = require('cors')
+
+const db = require('./data/db.json')
+
+const app = express()
+// const fs = require('fs')
+
+app.use(cors())
+
+app.get('/', (req,res) => {
+    res.status(200)
+    res.send(db)
+})
+
+
+module.exports = app
