9a105e7a0556ecd22cf8f38ae8123632edfc3a2c
Marwan Kabir <mkabir@mkabir-ltmwxcp.internal.salesforce.com>
2023-06-23 11:52:49 -0700

    Added backend for localhost

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -1,1 +1,14 @@
 // YOUR CODE HERE
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const db = require('./data/db.json')
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.status(200).send(db.products)
+})
+
+module.exports = app
