55a291951733abc308036ecab14f0a5c1e68968d
Avery Myart <amyart@amyart-ltm3ygh.internal.salesforce.com>
2023-06-23 09:32:41 -0700

    fixed grid layout

--- student-store-ui/src/components/ProductGrid/ProductGrid.jsx
+++ student-store-ui/src/components/ProductGrid/ProductGrid.jsx
@@ -3,33 +3,38 @@ import { Link } from "react-router-dom";
 import "./ProductGrid.css";
 
 export default function ProductGrid({ products, searchTerm, category }) {
-  const filteredProducts = products.filter(product => {
+  const filteredProducts = products.filter((product) => {
     if (category && product.category !== category) {
       return false;
     }
-    if (searchTerm && !product.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+    if (
+      searchTerm &&
+      !product.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ) {
       return false;
     }
     return true;
   });
 
   return (
-    <div className="prodgrid">
-      {filteredProducts.map((product) => 
-        <section className="products-grid" key={product.id}>
+    <div className="product-grid">
+      {filteredProducts.map((product) => (
+        <section className="product-card" key={product.id}>
           <Link to={`/product/${product.id}`}>
-            <div className="product-card">
-              <div className="product-img">
-                <img className="product-image" src={product.image} alt={product.name} />
-              </div>
-              <div className="product-info">
-                <p>{product.name}</p>
-                <p>{product.price}</p>
-              </div>
+            <div>
+              <img
+                className="product-image"
+                src={product.image}
+                alt={product.name}
+              />
+            </div>
+            <div className="product-info">
+              <p>{product.name}</p>
+              <p>{product.price}</p>
             </div>
           </Link>
         </section>
-      )}
+      ))}
     </div>
   );
 }
