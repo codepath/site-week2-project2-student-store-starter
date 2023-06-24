fa4f763176ecb8aaafc5b48309d50dc4daf3a54c
Iden Amoako <iamoako@iamoako-ltmuqpv.internal.salesforce.com>
2023-06-23 17:43:57 -0700

    changes made

--- student-store-express-api/app.js
+++ student-store-express-api/app.js
@@ -3,6 +3,8 @@ const express = require('express')
 const cors = require('cors')
 const db = require("./data/db.json")
 
+let products = [...db.products];
+
 
 const app = express()
 
@@ -12,7 +14,12 @@ app.use(express.json())
 app.get('/', (req,res) => {
     res.status(200)
     res.send(db)
+    
 })
 
+app.get('/products/:id', (req,res) => {
+    res.status(200)
+    res.send(products.filter(product => parseInt(product.id) === parseInt(req.params.id)))
+})
 
 module.exports = app
